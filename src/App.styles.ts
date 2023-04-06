b8a7b01a3896ac4b8fb3196d25c9c68c7e37b3ad
Alexandre Marques Bulcao <58255516+ambulcao@users.noreply.github.com>
2023-04-06 10:43:46 +0000

    Styling header

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -3,8 +3,20 @@ import styled from 'styled-components';
 export const Container = styled.div``;
 
 export const Header = styled.div`
+  background-color: #861ed6;
+  height: 9.375rem;
+  text-align: center;
 `;
 
-export const HeaderText = styled.h1``;
+export const HeaderText = styled.h1`
+  margin: 0;
+  padding: 0;
+  color: #FFF;
+  padding-top: 1.875rem;
+`;
 
-export const Body = styled.div``;
+export const Body = styled.div`
+  margin: auto;
+  max-width: 61.25rem;
+  margin-bottom: 3.125rem;
+`;
