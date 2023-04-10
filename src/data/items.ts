74c75e838bde839b300a46a215871c9aa05695bc
Alexandre Marques Bulcao <58255516+ambulcao@users.noreply.github.com>
2023-04-10 09:40:43 +0000

    Creating typing of the Itens

---
+++ src/data/items.ts
@@ -0,0 +1,9 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+  { date: new Date(2023, 4, 10), category: 'food', title: 'McDonalds', value: 32.12 },
+  { date: new Date(2023, 4, 10), category: 'food', title: 'Burger King', value: 28 },
+  { date: new Date(2023, 4, 10), category: 'rent', title: 'Aluguel Apt', value: 2300 },
+  { date: new Date(2023, 5, 18), category: 'salary', title: 'Salário ACME', value: 4500 },
+];
+
