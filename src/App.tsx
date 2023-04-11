6287c072e33683bfb25500966406d4b4f2fecd79
Alexandre Marques Bulcao <58255516+ambulcao@users.noreply.github.com>
2023-04-11 16:16:16 +0000

    Creating useState from List and Items

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,8 @@ import { getCurrentMonth } from './helpers/dateFilter';
 const App = () => {
 
   const [list, setList] = useState(items);
-  const [currentMonth, setCurrentMonth] = useState();
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
 
   return (
     <C.Container>
