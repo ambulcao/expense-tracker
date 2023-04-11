6f04cc72ea1a73c126db85dea423c69b9d9b1df8
Alexandre Marques Bulcao <58255516+ambulcao@users.noreply.github.com>
2023-04-11 15:11:36 +0000

    Creating function Month

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+import { useState } from 'react';
 import * as C from './App.styles';
+import { Item } from './types/Item';
+import { Category } from './types/Category';
+import { categories } from './data/categories';
+import { items } from './data/items';
+import { getCurrentMonth } from './helpers/dateFilter'; 
 
 const App = () => {
+
+  const [list, setList] = useState(items);
+  const [currentMonth, setCurrentMonth] = useState();
+
   return (
     <C.Container>
       <C.Header>
